aa638aa6c8b8e65abb0c1515913f4ff7f5961a24
mathieu.tp <mathieu.turgeon-pelchat@canada.ca>
2021-04-19 13:28:36 -0400

    validation log and fixes

--- validation.py
+++ validation.py
@@ -1,7 +1,7 @@
 import argparse
 from pathlib import Path
-from preprocess_glob import tile_list_glob, ImageInfo
-from utils import read_parameters
+from preprocess_glob import tile_list_glob
+from utils import read_parameters, CsvLogger
 import glob
 from dataclasses import dataclass
 import rasterio
@@ -21,7 +21,7 @@ class ImgValidated:
     bands_info: dict
 
 
-def err_to_table(err_list):
+def err_to_table(err_list, csv_log):
     str_log = f"*** List of images with errors and the detected error ***\n"
     for im in err_list:
         im_log = f"\t{im.img_name}  --->" \
@@ -29,9 +29,10 @@ def err_to_table(err_list):
 
         str_log += im_log
     print(str_log)
+    csv_log.write_row([str_log])
 
 
-def val_to_table(val_list):
+def val_to_table(val_list, csv_log):
     str_log = f"*** Statistics for Validated Images ***\n" \
               f"Image name and per band [min, max, mean, std]"
     for im in val_list:
@@ -42,6 +43,7 @@ def val_to_table(val_list):
                   f"\t\t\tNIR: {im.bands_info['N']}\n"
         str_log += img_log
     print(str_log)
+    csv_log.write_row([str_log])
 
 
 # Validation steps for the preprocess pipeline.
@@ -58,6 +60,7 @@ def main(glob_params: dict = None):
     logging.config.fileConfig(log_config_path)  # See: https://docs.python.org/2.4/lib/logging-config-fileformat.html
     logging.info("Validation Started")
 
+    CsvLog = CsvLogger(out_csv=glob_params['base_dir'] + '/validation.csv')
     pansharp_glob_list = tile_list_glob(**glob_params)
 
     error_list = []
@@ -65,22 +68,27 @@ def main(glob_params: dict = None):
     for img in tqdm(pansharp_glob_list, desc='Iterating through images'):
         bgrn_dict = {'B': '', 'G': '', 'R': '', 'N': ''}
 
-        lst_img = [name for name in glob.glob(str(img.parent_folder / img.image_folder / img.prep_folder) + "/*.tif")]
+        lst_img = [Path(name) for name in glob.glob(str(img.parent_folder / img.image_folder / img.prep_folder) + "/*.tif")]
+        lst_img.extend([Path(name) for name in glob.glob(str(img.parent_folder / img.image_folder / img.prep_folder) + "/*.TIF")])
         for b in bgrn_dict.keys():
-            p = rf"_uint8_BAND_{b}.tif"
-            bgrn_dict[b] = [el for el in lst_img if el.endswith(p)][0]
+            p = rf"_BAND_{b}.tif"
+            try:
+                bgrn_dict[b] = [el for el in lst_img if el.endswith(p)][0]
+            except IndexError:
+                err_img = ImgError(img_name=str(img.im_name), detected_error=f"Band {b} is missing.")
+                error_list.append(err_img)
 
-        img_info = ImgValidated(img_name=str(img.image_folder), bands_info={'B': [], 'G': [], 'R': [], 'N': []})
+        img_info = ImgValidated(img_name=str(img.im_name), bands_info={'B': [], 'G': [], 'R': [], 'N': []})
         for key, val in bgrn_dict.items():
             if val:
-                with rasterio.open(val, 'r') as dst:
+                with rasterio.open(str(val), 'r') as dst:
                     err_img = None
                     if not dst.crs:
-                        err_img = ImgError(img_name=str(img.image_folder), detected_error=f"No CRS found")
+                        err_img = ImgError(img_name=str(img.im_name), detected_error=f"No CRS found")
                         error_list.append(err_img)
                         break
                     elif dst.dtypes[0] != 'uint8':
-                        err_img = ImgError(img_name=str(img.image_folder), detected_error=f"Image dtype is {dst.dtype}")
+                        err_img = ImgError(img_name=str(img.im_name), detected_error=f"Image dtype is {dst.dtype}")
                         error_list.append(err_img)
                         break
                     else:
@@ -88,15 +96,11 @@ def main(glob_params: dict = None):
                         info_list = [np.min(array), np.max(array), round(float(np.mean(array)), 2), round(float(np.std(array)), 2)]
                         img_info.bands_info[key] = info_list
 
-            else:
-                err_img = ImgError(img_name=str(img.image_folder), detected_error=f"Could not locate all 4 BGRN tif files")
-                error_list.append(err_img)
-                break
         if not err_img:
             val_list.append(img_info)
 
-    err_to_table(error_list)
-    val_to_table(val_list)
+    err_to_table(error_list, csv_log=CsvLog)
+    val_to_table(val_list, csv_log=CsvLog)
 
 
 if __name__ == '__main__':
