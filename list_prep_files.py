fb9b578490be14b224e65a1420edb165b7c6ded3
mathieu.tp <mathieu.turgeon-pelchat@canada.ca>
2021-04-27 15:31:01 -0400

    filter by data type

--- list_prep_files.py
+++ list_prep_files.py
@@ -2,18 +2,28 @@
 from pathlib import Path
 import argparse
 import glob
+import re
 from preprocess_glob import tile_list_glob
 from utils import read_parameters, CsvLogger
 
 
-def main(glob_params):
+def main(glob_params, keep_only='all'):
     images_list = tile_list_glob(**glob_params)
     CsvLog = CsvLogger(out_csv=glob_params['base_dir'] + '/prep_img.csv')
     for img in images_list:
         lst_img = [Path(name) for name in glob.glob(str(img.parent_folder / img.image_folder / img.prep_folder) + "/*.tif")]
         lst_img.extend([Path(name) for name in glob.glob(str(img.parent_folder / img.image_folder / img.prep_folder) + "/*.TIF")])
         for elem in lst_img:
-            CsvLog.write_row([str(elem)])
+            if keep_only == 'all':
+                CsvLog.write_row([str(elem)])
+
+            elif keep_only == 'singleband':
+                if re.search(rf'*_BAND_*', str(elem.stem)):
+                    CsvLog.write_row([str(elem)])
+
+            else:
+                if re.search(rf'*_{keep_only}_*', str(elem.stem)):
+                    CsvLog.write_row([str(elem)])
 
 
 if __name__ == '__main__':
@@ -26,4 +36,4 @@ if __name__ == '__main__':
 
     log_config_path = Path('logging.conf').absolute()
 
-    main(glob_params=params['glob'])
+    main(glob_params=params['glob'], keep_only=params['list_img']['keep_only'])
